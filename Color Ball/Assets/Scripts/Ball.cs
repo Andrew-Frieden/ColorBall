7b8d9d8999680604651957647bce46c58a5066f4
Andrew Frieden <drew@ninjaparty.net>
2015-12-02 22:21:21 -0500

    Initial Project Commit

---
+++ Color Ball/Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    public BallColor Color;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        BallColorColliderController colorCollider = col.gameObject.GetComponent<BallColorColliderController>();
+
+        if (colorCollider != null && colorCollider.Color == Color)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public enum BallColor
+    {
+        Green,
+        Red,
+        Blue
+    }
+}
---
+++ Color Ball/Assets/Scripts/BallColorColliderController.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallColorColliderController : MonoBehaviour
+{
+    public Ball.BallColor Color;
+}
